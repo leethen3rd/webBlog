f463bbc29ebdb37158436de7b6e12fe92fc059c0
Lee <leethen3rd@gmail.com>
2020-02-18 23:08:37 -0500

    Added admin backend and static pages

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Michael Scott',
-        'title': 'Blog Post One',
-        'content': 'Nice to meet me!',
-        'date_posted': 'March 10, 1983',
-    },
-    {
-        'author': 'Jim Halpert',
-        'title': 'Blog Post Two',
-        'content': 'Ding Dong!',
-        'date_posted': 'March 11, 1983',
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
